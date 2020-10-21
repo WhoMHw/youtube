a1d2b38f1412860895d52713907449f83abb2819
WhoMHw <71681090+WhoMHw@users.noreply.github.com>
2020-10-21 15:53:21 +0700

    Add files via upload

---
+++ wibi.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+clear 
+ echo "════════════════════════════════════════════════════════════════════════════════════════════"
+ echo "					_Author WhoMHw_						  "
+ echo "			_tools gabut_								  "
+ echo "════════════════════════════════════════════════════════════════════════════════════════════"
+ sleep 2
+ echo "selamat datang kaka"
+ sleep 3
+ echo "siapa nama kaka?" #tulisan keluar
+ read nick #membaca tulisan 
+clear
+ echo "Hallo kaka" $nick
+ sleep 3 
+ echo "selamat datang ditools ini"
+ echo "Buka Wa"
+ termux-open https://wa.me/+6285336515046?text=kaka+aku+pinjam+tools+mu
+ clear
+echo "install tool dulu"
+ sleep 6
+ echo "##_10%"
+ sleep 2
+ echo "###_20%"
+ sleep 7
+ echo "####_30% "
+ sleep 5
+ echo "#####_40%"
+ sleep 3
+ echo "######_50%"
+ sleep 4
+ echo "#######_60%"
+ sleep 8
+ echo "########_70%"
+ sleep 1
+ echo "#########_80%"
+ sleep 9
+ echo "##########_90%"
+ sleep 8
+ echo "###########_100%"
+ sleep 2
+ echo "tools telah diinstall"
+ sleep 6
+clear
+ echo "done"
+ echo "Download youtube lewat termux"
+ sleep 3
+ echo "By WhoMHw"
+ sleep 3
+ echo "════════════════════════════════════════════════════════════════════════════════════════════"
+ cd /sdcard
+ wget https://pastebin.com/raw/LY5iibS5 -O yt.sh
+ bash yt.sh
+
